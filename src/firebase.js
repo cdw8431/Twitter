295ed3bfe2979cc13ce449595323a4cb08ee13e2
cdw8431 <cdw8431@gmail.com>
2021-02-20 19:06:06 +0900

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQhi0KkLMgdufTL2xoo-GmD0t3tAFg36o",
+  authDomain: "twitter-d3ca3.firebaseapp.com",
+  projectId: "twitter-d3ca3",
+  storageBucket: "twitter-d3ca3.appspot.com",
+  messagingSenderId: "998834131108",
+  appId: "1:998834131108:web:79070f6390aeed9153d631",
+};
+
+export default firebase.initializeApp(firebaseConfig);
