b245c7d4bd76c4770d420946c3e465e40e0927ff
cdw8431 <cdw8431@gmail.com>
2021-02-21 23:03:14 +0900

    네비게이션 및 로그아웃 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,21 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routers/Auth";
 import Home from "routers/Home";
+import Profile from "routers/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
             </Route>
           </>
         ) : (
